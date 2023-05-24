7b0dec52f3e9b8a844efc2dad1ec97540d75dc3a
Hrishita Panjetha <hrishita.panjetha@gmail.com>
2023-05-24 06:59:35 +0530

    Created using Colaboratory

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-import pandas as pd
-df = pd.read_csv('data.csv')
-df.head()
-#pd.plotting.scatter_matrix(df, alpha=0.1, figsize=(10,10), diagonal="kde");
-# race_count
-df.groupby('race')['race'].count()
-# average_age_men
-round(df.groupby('sex')['age'].mean()[1], 1)
-# percentage_bachelors
-round(df['education'].value_counts(normalize=True)['Bachelors'] * 100, 1)
-
-# higher_education
-# `Bachelors`, `Masters`, or `Doctorate`
-round(df['education'].value_counts(normalize=True)[['Bachelors', 'Masters', 'Doctorate']].sum() * 100, 1)
-# lower_education
-# not `Bachelors`, `Masters`, or `Doctorate`
-1 - df['education'].value_counts(normalize=True)[['Bachelors', 'Masters', 'Doctorate']].sum()
-# higher_education_rich
-df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]['salary'].value_counts(normalize=True)['>50K']
-round(df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]['salary'].value_counts(normalize=True)['>50K'] * 100, 1)
-# lower_education_rich
-round(df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]['salary'].value_counts(normalize=True)['>50K'] * 100, 1)
-# min_work_hours
-df['hours-per-week'].min()
-# num_min_workers
-df[df['hours-per-week'] == 1].shape[0]
-# rich_percentage
-round(df[df['hours-per-week'] == 1]['salary'].value_counts(normalize=True)['>50K'] * 100, 1)
-# highest_earning_country
-df.groupby('native-country')['salary'].value_counts(normalize=True).loc[:, ('>50K')].idxmax()
-# highest_earning_country_percentage
-round(df.groupby('native-country')['salary'].value_counts(normalize=True).loc[:, ('>50K')].max() * 100, 1)
-# top_IN_occupation
-df[(df['native-country'] == 'India') & (df['salary'] == '>50K')]['occupation'].value_counts().idxmax()
-
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPutNcUuV/YSaoRnsqWfNqi"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "id": "dg_5czQAW-X4"
+      },
+      "outputs": [],
+      "source": [
+        "import pandas as pd"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "df = pd.read_csv('data.csv')\n",
+        "df.head()"
+      ],
+      "metadata": {
+        "id": "Zx0lBuySXFN1"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "8bPsacAHXWkf"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
